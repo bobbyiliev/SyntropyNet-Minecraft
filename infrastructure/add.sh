0504704871a44bc89fdf23138546657f6925ebf8
Bobby Iliev <bobby@bobbyiliev.com>
2021-06-12 14:23:38 +0000

    Add the remove server script

--- infrastructure/add.sh
+++ infrastructure/add.sh
@@ -4,9 +4,10 @@
 # Load all ENV variables
 ##
 export $(cat /var/www/syntropynet/.env | xargs)
+infrastructure_dir=/var/www/syntropynet/infrastructure
 
-lock_file=/var/www/syntropynet/infrastructure/status.lock
-server_details=/var/www/syntropynet/infrastructure/server_details.temp
+lock_file=${infrastructure_dir}/status.lock
+server_details=${infrastructure_dir}/server_details.temp
 server_name=YXNkYXNkCg-syntropy-$(date +%s)
 
 ##
@@ -38,6 +39,39 @@ function new_server(){
 }
 
 function run_server(){
-    echo "Configuration in progress..." > ${lock_file}
-    ssh -i /var/www/syntropynet/config/id_rsa -o "StrictHostKeyChecking no" root@${new_droplet_ip} env SYNTROPY_AGENT_TOKEN=${SYNTROPY_AGENT_TOKEN} 'bash -s' < /var/www/syntropynet/infrastructure/minecraft_server.sh
+    echo "Server configuration in progress..." > ${lock_file}
+    ssh -i /var/www/syntropynet/config/id_rsa -o "StrictHostKeyChecking no" root@${new_droplet_ip} env SYNTROPY_AGENT_TOKEN=${SYNTROPY_AGENT_TOKEN} 'bash -s' < ${infrastructure_dir}/minecraft_server.sh
+    sleep 10
+}
+
+function syntropy_config(){
+    echo "Syntropy Network configuration in progress..." > ${lock_file}
+    export SYNTROPY_API_TOKEN=${SYNTROPY_ACCESS_TOKEN}
+    export SYNTROPY_API_SERVER=https://controller-prod-server.syntropystack.com
+
+    echo  "| Get the new server endpoint id " > ${lock_file}
+    current_server_id=$(syntropyctl get-endpoints | grep -w $(hostname) | awk '{ print $2 }')
+    new_server_id=$(syntropyctl get-endpoints | grep -w ${server_name} | awk '{ print $2 }')
+
+    echo  "| Connect the new endpoint with the newtwork " > ${lock_file}
+    syntropyctl manage-network-endpoints BungeeCord --add-endpoint ${new_server_id}
+    syntropyctl configure-endpoints ${new_server_id} --enable-all-services
+
+    echo  "| Configure the BungeeCord connection " > ${lock_file}
+    syntropyctl create-connections BungeeCord ${new_server_id} ${current_server_id}
+
+    echo  "| Get the new Minecraft Docker container IP " > ${lock_file}
+    docker_endpoint_ip=$(syntropyctl get-endpoints --id ${new_server_id} --show-services --json | grep agent_service_subnet_ip | awk '{ print $2 }' | sed 's/"//g' | sed 's/,//')
+}
+
+function proxy_config(){
+
+    cat ${infrastructure_dir}/temp.yaml | sed "s/S_NAME/${server_name}/g"  | sed "s/S_IP/${docker_endpoint_ip}/g" >> ${infrastructure_dir}/config.yml
+
+    echo  "| Realding the proxy " > ${lock_file}
+    screen -R 7261 -X stuff 'greload^M'
+}
+
+function main(){
+    # Call all functions
 }
---
+++ infrastructure/remove.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##
+# Accept the server name which needs to be deleted as an argument
+##
+
+#TODO
+
+##
+# Load all ENV variables
+##
+export $(cat /var/www/syntropynet/.env | xargs)
+infrastructure_dir=/var/www/syntropynet/infrastructure
+
+lock_file=${infrastructure_dir}/remove.lock
+
+function remove_from_proxy(){
+    echo "Removing server from proxy..." > ${lock_file}
+    sed -i -e  '/YXNkYXNkCg-asdasd/,+3d' ${infrastructure_dir}config.yml
+
+    echo "Restarting the proxy server..." > ${lock_file}
+    screen -R 7261 -X stuff 'greload^M'
+}
+
+function remove_from_syntropy(){
+    echo "Removing server from Syntropy network..." > ${lock_file}
+    new_server_id=$(syntropyctl get-endpoints | grep -w ${server_name} | awk '{ print $2 }')
+    manage-network-endpoints --remove-endpoint-with-connections ${endpoint_id}
+}
+
+function remove_from_digitalocean(){
+    echo "Deleting server from DigitalOcean..." > ${lock_file}
+    doctl auth init --access-token ${DO_API_KEY}
+    doctl compute droplet delete ${server_name}
+}
